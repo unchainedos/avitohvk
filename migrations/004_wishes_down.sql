5168cb265184e79151cb7684f8486a3ed3852408
max23 <golchanskiy23@gmail.com>
2026-08-06 09:45:45 +0300

    Created wishes and wish_items(many-to-many) tables migration

---
+++ migrations/004_wishes_down.sql
@@ -0,0 +1,8 @@
+drop index if exists idx_wish_items_item_id;
+
+drop table if exists wish_items;
+
+drop index if exists idx_wish_user_id;
+drop index if exists idx_wish_title;
+
+drop table if exists wishes;
