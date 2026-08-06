3a565e8fb97cc940916619b1617ff0baff33f8d3
max23 <golchanskiy23@gmail.com>
2026-08-06 09:15:02 +0300

    Created items with indexes table migration

---
+++ migrations/003_items_down.sql
@@ -0,0 +1,8 @@
+drop index if exists idx_author_id;
+drop index if exists idx_holder_id;
+drop index if exists idx_category;
+drop index if exists idx_locked_holder;
+
+drop table if exists items;
+
+
